07575ca4d0e2d5805641be67a5e8adb19630ee24
Adam <18970079817@189.cn>
2018-12-13 16:33:33 +0800

    Alpha.

--- Shodan_Crawler.py
+++ Shodan_Crawler.py
@@ -23,21 +23,21 @@ def check_ip_range(iprange):
                 print "IP address out of range."
                 sys.exit()
     try:
-        first_ip = iprange.split('-')[0]
+        start_ip = iprange.split('-')[0]
     except:
         print "First IP wrong IP format."
         sys.exit()
     try:
-        second_ip = iprange.split('-')[1]
+        end_ip = iprange.split('-')[1]
     except:
         print "Second IP wrong IP format."
         sys.exit()
-    check_ip(first_ip)
-    check_ip(second_ip)
-    if int(second_ip.split('.')[3]) <= int(first_ip.split('.')[3]):
-        if int(second_ip.split('.')[2]) <= int(first_ip.split('.')[2]):
-            if int(second_ip.split('.')[1]) <= int(first_ip.split('.')[1]):
-                if int(second_ip.split('.')[0]) <= int(first_ip.split('.')[0]):
+    check_ip(start_ip)
+    check_ip(end_ip)
+    if int(end_ip.split('.')[3]) <= int(start_ip.split('.')[3]):
+        if int(end_ip.split('.')[2]) <= int(start_ip.split('.')[2]):
+            if int(end_ip.split('.')[1]) <= int(start_ip.split('.')[1]):
+                if int(end_ip.split('.')[0]) <= int(start_ip.split('.')[0]):
                     print "Second IP is small than first IP."
                     sys.exit()
 
@@ -47,16 +47,23 @@ def get_ip(ipaddr):
 
 iprange = raw_input('Please input the ip range,\nsuch as xxx.xxx.xxx.xxx-xxx.xxx.xxx.xxx:')
 check_ip_range(iprange)
-first_ip = iprange.split('-')[0]
-second_ip = iprange.split('-')[1]
+start_ip = iprange.split('-')[0]
+end_ip = iprange.split('-')[1]
 
-r = requests.get('https://www.shodan.io/host/' + first_ip)
-#print r
-#print r.text
-soup = BeautifulSoup(r.text, 'lxml')
-for tbody in soup.find_all('tbody'):
-    for tr in soup.find_all('tr'):
-        print tr.find('td').string
-        print tr.find('th').string
-        
+int_ip = lambda x: '.'.join([str(x/(256**i)%256) for i in range(3,-1,-1)])
+ip_int = lambda x:sum([256**j*int(i) for j,i in enumerate(x.split('.')[::-1])])
 
+for i in range(ip_int(start_ip), ip_int(end_ip)+1):
+    try:
+        r = requests.get('https://www.shodan.io/host/' + int_ip(i))
+    except:
+        print 'Some errors happened, check your network or something else.'
+    else:
+        soup = BeautifulSoup(r.text, 'lxml')
+        for tbody in soup.find_all('tbody'):
+            for tr in soup.find_all('tr'):
+                print tr.find('td').string
+                print tr.find('th').string
+        for ul in soup.find_all('ul', class_ = 'ports'):
+            for li in ul.find_all('li'):
+                print li.find('a').string
